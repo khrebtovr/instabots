f20dd61f598bf809ca7688ab2bae4eb8970eb410
Ruslan <khrebtovr@gmail.com>
2021-06-24 15:51:13 +0300

    add six screen

---
+++ js/main.js
@@ -0,0 +1,24 @@
+        $(document).ready(function() {
+            $(".btn-structure").click(function() {
+                $(".remove-one").toggleClass("classtrue");
+                return false;
+            });
+        });
+        $(document).ready(function() {
+            $(".btn-structure").click(function() {
+                $(".remove-two").toggleClass("classtrue");
+                return false;
+            });
+        });
+        $(document).ready(function() {
+            $(".btn-structure").click(function() {
+                $(".remove-three").toggleClass("classtrue");
+                return false;
+            });
+        });
+        $(document).ready(function() {
+            $(".btn-structure").click(function() {
+                $(".remove-four").toggleClass("classtrue");
+                return false;
+            });
+        });
