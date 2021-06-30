0b43de840370186b8cc8d1c35e020c631fe2cc7d
Ruslan <khrebtovr@gmail.com>
2021-06-30 16:39:18 +0300

    tablet done2

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@
 
         var acc = document.getElementsByClassName("accordion");
         var i;
-
         for (i = 0; i < acc.length; i++) {
             acc[i].addEventListener("click", function() {
                 this.classList.toggle("active");
